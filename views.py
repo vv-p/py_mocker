72b14be60db86d61d82954767dffd0a138a7603f
Vladimir Ponarevsky <vvp@protonmail.ch>
2018-08-24 15:18:43 +0300

    Add url dispatcher to mock server

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
 # views.py
+import re
+
 from dataclasses import dataclass, field
 from aiohttp import web
 from collections import namedtuple
@@ -10,23 +12,57 @@ MockSignature = namedtuple('MockSignature', ['method', 'path'])
 class MockData:
     status: int = 200
     headers: dict = field(default_factory=lambda: {})
-    body: str = ''
+    body: str = 'MockData Placeholder'
     json: dict = field(default_factory=lambda: {})
     redirect: str = '/'
     timeout: int = 0
 
 
-test_signature = MockSignature('GET', '/dsfsdf/')
+test_signature = MockSignature('GET', '/user/.*?/campaign/.*?/status')
 MOCKS = {
     test_signature: MockData(),
 }
 
 
+def check_request(method, path):
+    for sig in MOCKS:
+        if sig.method == method and re.match(sig.path, path):
+            return MOCKS[sig]
+    return None
+
+
+def add(request):
+    """Add new mock"""
+    return True
+
+
+def remove(request):
+    """Remove existing mock"""
+    return True
+
+
+mock_methods = {
+    'add': add,
+    'remove': remove,
+}
+
+
 async def index(request):
-    signature = MockSignature(request.method, request.path)
-    if signature not in MOCKS:
-        MOCKS[signature] = MockData()
-        text = 'Unknown resource {}'.format(request.method)
-    else:
-        text = 'Well-known resource {}'.format(request.method)
+    mock_data = check_request(request.method, request.path)
+    if mock_data:
+        text = mock_data.body
+        return web.Response(text=text)
+
+    if 'py-mocker' not in request.headers:
+        text = 'Unknown resource: {0.method} {0.path}'.format(request)
+        return web.Response(text=text)
+
+    mocker_command = request.headers['py-mocker']
+
+    if mocker_command not in mock_methods:
+        text = 'Unknown mocker command'
+        return web.Response(text=text)
+
+    mock_methods[mocker_command](request)
+    text = 'Do some shit'
     return web.Response(text=text)
